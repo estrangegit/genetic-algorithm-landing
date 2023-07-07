04f215c31b06fbdbd637fbd601bb02445bdaaad5
Etienne Estrangin <estrangin@actoll.com>
2023-07-07 12:35:43 +0200

    Correct configuration to watch template file changes as well

--- src/public/js/land.ts
+++ src/public/js/land.ts
@@ -1,6 +1,6 @@
 $(function(){
-  const CANVAS_WIDTH = 1000;
-  const CANVAS_HEIGHT = 500;
+  const CANVAS_WIDTH = 6999;
+  const CANVAS_HEIGHT = 5000;
 
   const $landCanvas = $('.land');
   const $startButton = $('.start');
@@ -10,8 +10,9 @@ $(function(){
 
   if(ctx != null) {
     $startButton.on('click', () => {
-      ctx.moveTo(0, 400);
-      ctx.lineTo(1000, 400);
+      ctx.lineWidth = 10;
+      ctx.moveTo(0, 4000);
+      ctx.lineTo(6999, 4000);
       ctx.stroke();
     })
   
@@ -19,4 +20,5 @@ $(function(){
       ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     })
   }
+
 })
